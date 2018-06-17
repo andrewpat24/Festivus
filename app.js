4b61ade44397a01123d0a6b25bc36d3d045dcab0
Andrew Patterson <apat24@mail.sfsu.edu>
2018-06-16 22:27:35 -0700

    configured dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,6 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+console.log(process.env.DB_USERNAME);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
