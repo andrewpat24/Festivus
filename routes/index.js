308f230952cfbb9f5415372791500f4432f77776
cameronobrien <cameron.o.j@gmail.com>
2018-07-18 20:35:29 -0500

    idk

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,6 @@ const artists = require('../db/helperFunctions/artists');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 
 
-
-
 // Passport session setup
 /* To support persistent login sessions, Passport needs to be able to serialize users into and deserialize
 users out of the session. This will be as simple as storing the user ID when serializing, and finding the user by
