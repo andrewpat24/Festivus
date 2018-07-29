03cea0f2ef24c20b652369b8e31f06cac137a9f0
Andrew Patterson <apat24@mail.sfsu.edu>
2018-07-28 21:00:55 -0700

    stash changes

--- server/models/festivals.js
+++ server/models/festivals.js
@@ -3,11 +3,19 @@ module.exports = (sequelize, DataTypes) => {
   var festivals = sequelize.define('festivals', {
     name: DataTypes.STRING,
     show_url: DataTypes.STRING,
-    location: DataTypes.STRING,
+    full_location: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state_region: DataTypes.STRING,
+    lat_long: DataTypes.STRING,
     date_span: DataTypes.STRING,
     bio: DataTypes.STRING,
     logo: DataTypes.STRING,
-    genre: DataTypes.STRING
+    genre: DataTypes.STRING,
+    twitter_url: DataTypes.STRING,
+    insta_url: DataTypes.STRING,
+    facebook_url: DataTypes.STRING,
+    view_count: DataTypes.INT,
+    follower_count: DataTypes.INT
   }, {});
   festivals.associate = function(models) {
     // associations can be defined here
