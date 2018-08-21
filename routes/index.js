9ed692513e91b838b7a3c52bb1db53f4c7f86b61
Andrew Patterson <apat24@mail.sfsu.edu>
2018-08-20 21:16:28 -0700

    cleaned up error page

--- routes/index.js
+++ routes/index.js
@@ -88,24 +88,28 @@ router.get('/festival/:festivalID', function(req, res, next) {
         return [
           {
             artistName: "David Guetta",
+            artistId:1,
             artistProfilePicture: "https://i.scdn.co/image/f65af663cd7c12c4adb4e9029767537f2bb50ad1",
             artistFollowers: 2345342, 
             genre: "EDM"
           },
           {
             artistName: "Zhu",
+            artistId:2,
             artistProfilePicture: "https://i.scdn.co/image/62225a86b462fca0a9f6a698fa6e4583f25bc0b7",
             artistFollowers: 2345234, 
             genre: "EDM"
           },
           {
             artistName: "Mt Eden",
+            artistId:3,
             artistProfilePicture: "https://i.scdn.co/image/6f0354b3d536679363bfa891f975fa9b2683666f",
             artistFollowers: 2345, 
             genre: "EDM"
           },
           {
             artistName: "Bassnectar",
+            artistId:4,
             artistProfilePicture: "https://i.scdn.co/image/b2d54418b81eea1a53663d8c9b63336a9991a50e",
             artistFollowers: 234523, 
             genre: "EDM"
@@ -114,6 +118,7 @@ router.get('/festival/:festivalID', function(req, res, next) {
       })(returnedFestival.id);  
       console.info(festivalLineup);
       res.render('festival', {
+        festivalId: returnedFestival.id,
         festivalName: returnedFestival.name,
         // festivalProfilePicture: returnedFestival.logo,
         festivalProfilePicture:'https://www.sfoutsidelands.com/uploads/ol18-outside-lands-logo-2018-large.png',
@@ -146,7 +151,7 @@ function ensureAuthenticated(req, res, next){
   res.redirect('/');
 } 
 
-router.get('/artist', function (req,res, next) {
+router.get('/artist/:artistId', function (req,res, next) {
   // var id = request.params.festivalID; 
   var artistAlbums = [
     'https://i.scdn.co/image/62225a86b462fca0a9f6a698fa6e4583f25bc0b7',
@@ -155,6 +160,7 @@ router.get('/artist', function (req,res, next) {
   ]
   res.render('artist', {
     artistName: 'Zhu',
+    artistId: 1,
     artistProfile: 'https://i.scdn.co/image/62225a86b462fca0a9f6a698fa6e4583f25bc0b7',
     artistBio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac vestibulum ante, eget tristique massa. Nullam commodo est nisl, a scelerisque dolor egestas quis. Curabitur commodo dolor ut interdum pellentesque. Duis cursus nisi quis tellus pulvinar, fermentum porttitor purus vestibulum. Duis sollicitudin, ante eget luctus rhoncus, felis arcu pretium enim, vel facilisis tellus nulla id nisl. Duis pharetra aliquet lobortis. Sed ut pretium tortor. Aenean aliquam ligula purus, nec porta tortor pellentesque maximus. Vestibulum interdum scelerisque tincidunt. Etiam id orci ac tortor commodo placerat nec in quam. Duis a erat sed felis placerat euismod. Morbi commodo aliquam ornare. Nulla pulvinar, orci sed gravida mattis, quam ipsum pharetra ipsum, et tempor sapien diam vitae odio. Proin sit amet nulla ac nulla lobortis luctus eget id eros. Morbi venenatis, est sit amet vulputate convallis, lacus ex euismod ante, ac ultrices ligula massa sed massa.',
     relatedArtists: [
